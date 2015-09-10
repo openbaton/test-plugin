17e571b654f0d8df7e41b5842b8d09eb0d209468
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-09-10 12:14:46 +0200

    adding mainclass

---
+++ src/main/java/org/project/openbaton/plugin/vim/drivers/test/Starter.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015. Fraunhofer FOKUS
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.project.openbaton.plugin.vim.drivers.test;
+
+import org.project.openbaton.plugin.PluginStarter;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by lto on 10/09/15.
+ */
+public class Starter {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try {
+            properties.load(Starter.class.getResourceAsStream("/plugin.conf.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        PluginStarter.run(TestClient.class, "test-plugin", properties.getProperty("registry-ip", "localhost"));
+    }
+}
